8ff78b8e283f19b2c5da5467fc5636786a4d9156
copleykj <copleykj@gmail.com>
2016-10-09 00:52:02 -0400

    implement method chaining :-)

--- odometer.cpp
+++ odometer.cpp
@@ -7,41 +7,31 @@ using namespace std;
 
 class Odometer{
     public:
-    void setOdometer(int distance);       // resets to 0
-    void setFuelEff(int fuelEff);         //sets fuel efficiency
-    void setAddMiles(int milesTraveled);  //adds miles to the odometer
-    void print();                         //Print
+        Odometer setOdometer(int distance){
+            trackDistance = distance;
+            return *this;
+        };       // resets to 0
+        Odometer setFuelEff(int fuelEff){
+            trackFuelEff = fuelEff;
+            return *this;
+        };         //sets fuel efficiency
+        Odometer setAddMiles(int milesTraveled){
+            trackDistance = milesTraveled + trackDistance;
+            return *this;
+        };  //adds miles to the odometer
+        Odometer print(){
+            cout << "Fuel used: " << getGallons() << " gallons" << endl;
+            cout << "Distance Traveled: " << trackDistance << " miles" <<endl;
+            return *this;
+        };                         //Print
 
     private:
-    double getGallons();                  // returns gallons used
-    double trackFuelEff;                   //Track mpg
-    int trackDistance;                     //Track miles driven
+        double getGallons(){
+            return trackDistance / trackFuelEff;
+        };                  // returns gallons used
+        double trackFuelEff;                   //Track mpg
+        int trackDistance;                     //Track miles driven
 };
-
-void Odometer::setOdometer(int distance){
-    trackDistance = distance;
-    return;
-}
-
-void Odometer::setFuelEff(int fuelEff){
-    trackFuelEff = fuelEff;
-    return;
-}
-
-void Odometer::setAddMiles(int milesTraveled){
-    trackDistance = milesTraveled + trackDistance;
-    return;
-}
-
-double Odometer::getGallons(){
-    return trackDistance / trackFuelEff;
-}
-
-void Odometer::print(){
-    cout << "Fuel used: " << this->getGallons() << " gallons" << endl;
-    cout << "Distance Traveled: " << trackDistance << " miles" <<endl;
-    return;
-}
 
 
 int main()
@@ -49,18 +39,8 @@ int main()
     Odometer trip1;
     Odometer trip2;
 
-    trip1.setOdometer(0); //sets odometer to 0
-    trip2.setOdometer(0); //sets odometer to 0
-
-    trip1.setFuelEff(29.9); //sets fuel efficiency
-    trip2.setFuelEff(15.3); //sets fuel efficiency for 2nd object
-
-    trip1.setAddMiles(100); //Tracks how many miles have been travelled
-    trip2.setAddMiles(142); //Tracks how many miles have been travelled for 2nd object
-
-
-    trip1.print();
-    trip2.print();
+    trip1.setOdometer(0).setFuelEff(29.9).setAddMiles(100).print();
+    trip2.setOdometer(0).setFuelEff(15.3).setAddMiles(142).print();
 
 
     return 0;
